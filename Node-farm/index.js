d3ad442efd0b1b428dcbf1e0b268893c517da38c
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-02 15:36:43 -0400

    Writing file

--- Node-farm/index.js
+++ Node-farm/index.js
@@ -5,6 +5,10 @@ const fs = require('fs');
 // UTF-8 - will not cause the buffer and read it staright in english
 const txtIn = fs.readFileSync('./txt/input.txt','utf-8')
 console.log(txtIn)
+
+const txtOut = `This is an expermient : ${txtIn}\n Added text ON ${Date.now()}`
+fs.writeFileSync('./txt/output.txt',txtOut);
+console.log('Txt added')
 //  const hello = 'Hello world'
 // console.log(hello)
 
