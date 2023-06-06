e217d8868e50a8588fd91104c7d59ed668314d22
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-06 02:41:06 -0400

    Improving the page functionality

--- Node-farm/index.js
+++ Node-farm/index.js
@@ -40,12 +40,31 @@ const url = require('url') ;
 
 // top level is executed only once when code is executed
 // will read the data and parse the json as a a string 
+const replaceTemplate = (temp , product) => {
+    // g - makes it global - for the mentioned item 
+    let output = temp.replace(/{%ID%}/g,product.id) ; 
+        output = output.replace(/{%PRODUCTNAME%}/g,product.productName) ; 
+        output = output.replace(/{%IMAGE%}/g,product.image); 
+        output = output.replace(/{%FROM%}/g,product.from) ; 
+        output = output.replace(/{%NUTRIENTS%}/g,product.nutrients) ; 
+        output = output.replace(/{%QUANTITY%}/g,product.quantity) ; 
+        output = output.replace(/{%PRICE%}/g,product.price) ; 
+        output = output.replace(/{%DESCRIPTION%}/g,product.description) ; 
+        
+        if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic') ; 
+        return output ; 
+    
+
+}
+
+
 const tempOverview  = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
-// const tempCard = fs.readFileSync(`${__dirname}/templates/template-card`,'utf-8');
-// const tempProduct = fs.readFileSync(`${__dirname}/product/template-product`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
 
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+// Dataaobj -> array -> data(json format)
 const dataObj  = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
@@ -53,7 +72,13 @@ const server = http.createServer((req, res) => {
     // overview page
     if(pathName === '/' || pathName === '/overview'){
         res.writeHead(200,{'Content-type':'text/html'})
-        res.end(tempOverview);
+        // map - for each element - something is returned
+        // looping - over the obj
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard ,el )).join('') 
+        const output = tempOverview.replace('{%PRODUCT_CARD%}',cardsHtml)
+        
+
+        res.end(output);
     // PRODUCT PAGE 
     }else if(pathName === '/product'){
         res.end('This is the product')
