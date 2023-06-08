481bde72d0c97b43c7b1e453b7509af700e724df
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-08 14:53:39 -0400

    Using crypto module

--- How-node-works/event-loop.js
+++ How-node-works/event-loop.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const crypto = require('crypto');
+
+const start = Date.now();
 // set timer - using arrow function 
 // not running inside I/O loop
 setTimeout(() => console.log("TImer 1 finished"), 0);
@@ -10,8 +13,19 @@ fs.readFile('test-file.txt' , () =>{
     console.log("------------------");
 
     setTimeout(() => console.log("TImer 2 finished"), 0);
+    // event loop will wait for the time specirfid
     setTimeout(() => console.log("TImer 3 finished"), 3000);
+    // will occurs immediately 
     setImmediate(() => console.log("Immediate 2 finished") , 0);
-})
 
-console.log("Hello from top-level code")
+    // part of micro task - gets run by the next phase(meaning the end of the last phase)
+    process.nextTick(() => console.log("Process.nextTick"));
+
+    // using crypto module - length , iteration and how strong it will be 
+    crypto.pbkdf2('password', 'salt', 100000, 528, 'sha512', () => {
+        console.log(Date.now(), 'Password encrypted');
+    });
+    
+});
+
+console.log("Hello from top-level code");
