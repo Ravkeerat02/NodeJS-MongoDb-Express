aa9574154971bc039acdba54af0d258788a0984d
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-09 02:35:53 -0400

    Playing with modules

---
+++ How-node-works/modules.js
@@ -0,0 +1,19 @@
+// console.log(arguments);
+// console.log(require("module").wrapper);
+
+// module.exports
+const C = require("./test-module1");
+const calc1 = new C();
+console.log(calc1.add(2, 5));
+console.log('************************')
+console.log(calc1.divide(1000,50));
+console.log('************************')
+// exports
+// const calc2 = require("./test-module-2");
+const { add, multiply } = require("./test-module2");
+console.log(multiply(2, 5));
+
+// caching
+require("./test-module3")();
+require("./test-module3")();
+require("./test-module3")();
