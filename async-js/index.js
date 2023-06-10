dc27f2793559438ca07f1903212ff2be3272e12f
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-10 00:27:21 -0400

    Improving code

--- async-js/index.js
+++ async-js/index.js
@@ -1,28 +1,53 @@
 const fs = require('fs');
+const { resolve } = require('path');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+// Promise function takes in an executor function (asynchronous code)
+const readFilePro = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      // Result of the error handler
+      if (err) reject('Could not find it');
+      resolve(data);
+    });
+  });
+};
+
+// creating Promise function - takes in file and thing thats been worked upon
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, err => {
+            if(err) reject('Nothing was found');
+            resolve('Success . WE FOUND IT') ;
+        });
+    });
+};  
+        
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then(data => {
     console.log(`Breed: ${data}`);
 
-    // returns a promise - pending promise(no data returned)
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
-        .then(res => {
-            console.log(res.body.message);
+    // Returns a promise - pending promise (no data returned)
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+    })
+  .then(res => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message);
 
-            fs.writeFile('dog-img.txt', res.body.message, err => {
-                if (err) {
-                    console.log('Error writing to file');
-                    return;
-                }
-                console.log('Dog saved');
-            });
-        })
-        // us3ed for returninh an error 
-        .catch(err => {
-            console.log(err.message);
-        });
-}); 
-
-// Promise - a value that will occur(appear) in the future
-// then - succesful - catch - unsuccessful
-// returned prmose(also called resolved promise) - can either be accepted or reject3ed
+    // fs.writeFile('dog-img.txt', res.body.message, err => {
+    //   if (err) {
+    //     console.log('Error writing to file');
+    //     return;
+    //   }
+    //   console.log('Dog saved');
+    // });f
+  })
+// Used for returning an error
+    .then(()=>{
+        console.log('IMAGE SAVED'); 
+        
+    }) 
+    .catch(err => {
+        console.log(err.message);
+  });
