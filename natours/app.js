0ac5500053fd2c078899c0045e32874b7273f5a7
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-10 02:34:33 -0400

    Setting up API(GET)

--- natours/app.js
+++ natours/app.js
@@ -1,14 +1,23 @@
 const express = require('express'); 
+const fs = require('fs'); // file system module
 const app = express();
 const port = 3000 ;
 
-// setting up route
-app.get('/',(req , res) =>{
-    res.status(200).json({message:'Welcome to the server'})
-})
+// // setting up route
 
-app.post('/',(req , res) =>{
-    res.status(200).json({message:'Welcome to the server'})
+// reding the file data - converting the file format
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+// should specify version of API(if being used)
+app.get('/api/v1/tours',(req, res)=>{
+    res.status(200).json({
+        status: 'success',
+        // makes sense with more data(multipe obj)
+        results: tours.length,
+        data: {
+            // will conatin teh data
+            tours: tours
+        }
+    })
 })
 
 // starts the server
