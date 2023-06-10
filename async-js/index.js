2204555ddfdad6d34cc9cb73b92062b5b6f90c13
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-10 00:06:47 -0400

    Playing with promise

--- async-js/index.js
+++ async-js/index.js
@@ -4,17 +4,25 @@ const superagent = require('superagent');
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     console.log(`Breed: ${data}`);
 
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err ,res) =>{
-    if(err) return console.log(err.message);
-        console.log(res.body.message);
+    // returns a promise - pending promise(no data returned)
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+        .then(res => {
+            console.log(res.body.message);
 
-    fs.writeFile('dog-img.txt', res.body.message, err =>{
-        console.log('Dog saved');
-    })
-});
+            fs.writeFile('dog-img.txt', res.body.message, err => {
+                if (err) {
+                    console.log('Error writing to file');
+                    return;
+                }
+                console.log('Dog saved');
+            });
+        })
+        // us3ed for returninh an error 
+        .catch(err => {
+            console.log(err.message);
+        });
+}); 
 
-
-});
-
-
-
+// Promise - a value that will occur(appear) in the future
+// then - succesful - catch - unsuccessful
+// returned prmose(also called resolved promise) - can either be accepted or reject3ed
