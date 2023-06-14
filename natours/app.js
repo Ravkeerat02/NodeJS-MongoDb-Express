2f1bf34813032f06f73738035c4243d0748f72ce
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-14 01:51:29 -0400

    Playing with del req

--- natours/app.js
+++ natours/app.js
@@ -83,6 +83,21 @@ app.patch('/api/v1/tours/:id', (req, res) =>{
     })
 })
 
+// deleting the data
+app.delete('/api/v1/tours/:id', (req, res) =>{
+    if(req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    // 204 - no content presrnt
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+});
+
 
 // starts the server
 app.listen(port , () =>{
