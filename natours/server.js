0d001ca661b94fa8cc2a89ae30f74c4890157d75
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-16 03:02:19 -0400

    Ckeaning up code

--- natours/server.js
+++ natours/server.js
@@ -12,35 +12,6 @@ mongoose.connect(DB , {
   useFindAndModify: false
 }).then(() => console.log('DB connection successful!'));
 
-// creating model - describing(validator)
-const tourSchema = new mongoose.Schema({
-    name : {
-      type : String,
-      required : [true , 'A tour must have a name'],
-    }, 
-    price :{
-      type : Number,
-      required : [true , 'A tour must have a rating']
-    }, 
-    rating : {
-      type: Number , 
-      default : 4.5 
-    }
-
-  }) ; 
-  const Tour = mongoose.model('Tour' , tourSchema) ;
-
-  const testTour = new Tour({
-    name : 'TTester',
-    price : 12500, 
-    rating : 1.5
-  }) ;
-
-  testTour.save().then(doc => {
-    console.log(doc) ;
-  }).catch(err => {
-    console.log('ERROR : ' , err) ;
-  })
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
