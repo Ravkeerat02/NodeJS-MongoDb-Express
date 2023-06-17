698cd6ecdaafec09fa0990c347c88874a08e10dd
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-17 02:10:27 -0400

    Filtering works

--- natours/controller/tourcontroller.js
+++ natours/controller/tourcontroller.js
@@ -2,22 +2,34 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    // Find all the tours in the database
-    const tours = await Tour.find();
+    // basic filter
+    const queryObj = { ...req.query };
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    // advanced filter
+    // converting obj to string
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    const query = Tour.find(JSON.parse(queryStr));
+
+    const tours = await query; 
+
     res.status(200).json({
       status: 'success',
       results: tours.length,
       data: {
-        tours
-      }
+        tours,
+      },
     });
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err.message
+      message: err.message,
     });
   }
 };
+
 
 exports.getTour = async (req, res) => {
   try {
