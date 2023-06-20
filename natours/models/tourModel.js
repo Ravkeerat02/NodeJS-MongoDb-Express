0ae8827cb88d1db06d73928fd7ba3440fad2d28d
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-20 02:43:02 -0400

    Middleware

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -6,6 +7,9 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     trim: true,
     unique: true
+  },
+  slug:{
+    type: String
   },
   duration: {
     type: Number,
@@ -59,10 +63,25 @@ const tourSchema = new mongoose.Schema({
 
 // setting up virtual properties - CANT BE YSED IN QUERY - belongs to the console not the controller
 // arrow functin cant use this directly but a regular dunction can 
+// middleware - can either be before or after a fucntion is being called and processed
 tourSchema.virtual('durationWeek').get(function(){
   return this.duration / 7 ; 
 })
 
+// // PRE/POST HOOKS - mongoose middleware - runs before save and create 
+// tourSchema.pre('save',function(next){
+//   this.slug = slugify(this.name , {lower: true})
+//   // will refer to next middleware
+//   next()
+// });
+
+// tourSchema.post('save',function(doc , next){
+//   console.log(doc);
+//   next()
+// })
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
+
+
