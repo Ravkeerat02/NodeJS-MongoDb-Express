03d09c43853792087f34f8a56676bf83c8d33bb4
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-20 00:37:50 -0400

    Improving the code

--- natours/controller/tourcontroller.js
+++ natours/controller/tourcontroller.js
@@ -1,66 +1,79 @@
 const Tour = require('./../models/tourModel');
 
-// calling it out
-    // query = query.skip(skip).limit(limit)
-    // alias - route(shortcut 
+// Calling it out
 exports.aliasTopTour = (req, res, next) => {
   req.query.limit = '5'; 
   req.query.sort = '-ratingsAverage,price';
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
+};
+
+// Creating a constructor function
+class APIFeatures {
+  constructor(query, queryString) {
+    Object.assign(this, { query, queryString });
+  }
+
+  filter(){
+    const queryObj = { ...this.queryString };
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    // Converting obj to string
+    let queryStr = JSON.stringify(queryObj);
+    // Filters on the basis of the specified text 
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // Stores the query obj - which is being used for different operations
+    this.query = this.query.find(JSON.parse(queryStr));
+    // Returns the entire obj
+    return this;
+  }
+
+  sort(){
+    if(this.queryString.sort){
+      // Returns array of all the name
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+    return this; 
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      // Everything except this field
+      this.query = this.query.select('-__v');
+    }
+    return this;
+  }
+
+  paginate(){
+    const page = this.queryString.page * 1 || 1; 
+    const limit = this.queryString.limit * 1 || 100; 
+    // Skips the page based on the page and limit
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit); 
+    return this ; 
+  }
 }
 
 exports.getAllTours = async (req, res) => {
   try {
-    // basic filter
-    const queryObj = { ...req.query };
-    const excludeFields = ['page', 'sort', 'limit', 'fields'];
-    excludeFields.forEach((el) => delete queryObj[el]);
+    // Executes query 
+    // Runs code for API functionality
+    // It gets returned because of using `this` keyword
+    const features = new APIFeatures(Tour.find(), req.query).
+    filter()
+    .sort()
+    .limitFields()
+    .paginate();
+    const tours = await features.query; 
 
-    // advanced filter
-    // converting obj to string
-    let queryStr = JSON.stringify(queryObj);
-    // filters on the basis of the specified text 
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    // stores the query obj - which is being used for different operations
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // Sorting 
-    if(req.query.sort){
-      // returns array of all the name
-      const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
-    }else{
-      query = query.sort('-createdAt')
-    }
-
-    // filed limiting - limiting the amount of data returned
-    if(req.query.fields){
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    }else{
-      // everything except this filed
-      query = query.select('-__v')
-    }
-
-    // pagination - just selecting(viewing) specifc page at time
-    // converts string to a number
-    const page = req.query.page * 1 || 1; 
-    const limit = req.query.limit * 1 || 100; 
-    // skips the page based on the page and limit
-    const skip = (page - 1 ) * limit
-
-    query = query.skip(skip).limit(limit); 
-    if(req.query.page){
-      const numTours = await Tour.countDocumnets();
-      if(skip >= numTours) throw new Error("Page doesnt exist")
-    }
-    
-
-
-
-    const tours = await query; 
-
+    // Sends response
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -76,20 +89,19 @@ exports.getAllTours = async (req, res) => {
   }
 };
 
-
 exports.getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id);
     res.status(200).json({
       status: 'success',
       data: {
-        tour
-      }
+        tour,
+      },
     });
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err.message
+      message: err.message,
     });
   }
 };
