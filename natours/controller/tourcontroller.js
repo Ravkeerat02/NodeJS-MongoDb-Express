b4b0838dc48e47e1f1296101837a17390b37f4f6
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-20 00:41:18 -0400

    Orgazning code

--- natours/controller/tourcontroller.js
+++ natours/controller/tourcontroller.js
@@ -1,4 +1,5 @@
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 // Calling it out
 exports.aliasTopTour = (req, res, next) => {
@@ -9,57 +10,7 @@ exports.aliasTopTour = (req, res, next) => {
 };
 
 // Creating a constructor function
-class APIFeatures {
-  constructor(query, queryString) {
-    Object.assign(this, { query, queryString });
-  }
 
-  filter(){
-    const queryObj = { ...this.queryString };
-    const excludeFields = ['page', 'sort', 'limit', 'fields'];
-    excludeFields.forEach((el) => delete queryObj[el]);
-    // Converting obj to string
-    let queryStr = JSON.stringify(queryObj);
-    // Filters on the basis of the specified text 
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    // Stores the query obj - which is being used for different operations
-    this.query = this.query.find(JSON.parse(queryStr));
-    // Returns the entire obj
-    return this;
-  }
-
-  sort(){
-    if(this.queryString.sort){
-      // Returns array of all the name
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort('-createdAt');
-    }
-    return this; 
-  }
-
-  limitFields() {
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
-      this.query = this.query.select(fields);
-    } else {
-      // Everything except this field
-      this.query = this.query.select('-__v');
-    }
-    return this;
-  }
-
-  paginate(){
-    const page = this.queryString.page * 1 || 1; 
-    const limit = this.queryString.limit * 1 || 100; 
-    // Skips the page based on the page and limit
-    const skip = (page - 1) * limit;
-
-    this.query = this.query.skip(skip).limit(limit); 
-    return this ; 
-  }
-}
 
 exports.getAllTours = async (req, res) => {
   try {
