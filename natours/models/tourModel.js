1367283d1d9a715ffec3a6cba8763447156c3b1e
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-20 03:25:18 -0400

    Playing with validation

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -1,69 +1,100 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    trim: true,
-    unique: true
+const validator = require('validator');
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      maxLength: [40, 'A tour name must have less or equal than 40 characters'],
+      minLength: [10, 'A tour name must have more or equal than 10 characters'],
+      trim: true,
+      unique: true,
+      validate: [
+        {
+          validator: function(value) {
+            // Custom validation to check the actual length
+            return value.length >= 10 && value.length <= 40;
+          },
+          message: 'Tour name must be 10-40 characters long'
+        },
+        {
+          validator: function(value) {
+            // Additional validation to check if name has only letters
+            return validator.isAlpha(value);
+          },
+          message: 'Tour name must only contain letters'
+        }
+      ]
+    },
+    slug: {
+      type: String
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty']
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a rating']
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function(val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price'
+      }
+    },
+    summary: {
+      type: String,
+      required: [true, 'A tour must have a summary']
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
-  slug:{
-    type: String
-  },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration']
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size']
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty']
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a rating']
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    required: [true, 'A tour must have a summary']
-  },
-  description: {
-    type: String,
-    trim: true
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image']
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now()
-  },
-  startDates: [Date],
-  secretTour :{
-    type : Boolean , 
-    default : false 
+  {
+    // for the options
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
   }
-},{
-  // for the options
-  toJSON :  {virtuals: true},
-  toObject : {virtuals: true}
-
-});
+);
 
 // DOCUMENT MIDLWARE
 // setting up virtual properties - CANT BE YSED IN QUERY - belongs to the console not the controller
