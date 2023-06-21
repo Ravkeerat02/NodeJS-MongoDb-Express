fbfa2ab3324780cfc07e3ecc49cc8894681d4f49
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-21 03:58:56 -0400

    Refactoring error handler

--- natours/controller/tourcontroller.js
+++ natours/controller/tourcontroller.js
@@ -56,23 +56,22 @@ exports.getTour = async (req, res) => {
     });
   }
 };
-
-exports.createTour = async (req, res) => {
-  try {
+// using this for simplying the error handling
+catchAsync = fn => {
+  return (req , res , next) => {
+    fn(req,res,next).catch(next);
+    };
+  }
+// will return promise , will result in catch if its rejected(not approved)
+exports.createTour = catchAsync(async (req, res) => {
     const newTour = await Tour.create(req.body);
     res.status(201).json({
       status: 'success',
       data: {
         tour: newTour
       }
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err
-    });
-  }
-};
+    });  
+});
 
 exports.updateTour = async (req, res) => {
   try {
