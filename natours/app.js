d76a968767aff7face1160f9bbb774822c65b4a6
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-21 03:13:31 -0400

    Improving error handling

--- natours/app.js
+++ natours/app.js
@@ -25,11 +25,27 @@ app.use('/api/v1/tours', tourRouter);
 
 // error handling - for everything - not relatedd to the above routes
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-      status: 'fail',
-      message: `Can't find ${req.originalUrl} on this server!`
-    });
+    // res.status(404).json({
+    //   status: 'fail',
+    //   message: `Can't find ${req.originalUrl} on this server!`
+    // });
+    // using built in function to create an error
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.status = 'fail';
+    err.statusCode = 404;
+    // passing error to the next function
+    next(err);
   });
 
+// global error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.status || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message : err.message
+  })
+  res.status();
+});
 
 module.exports = app;
