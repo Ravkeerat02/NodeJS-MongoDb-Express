6fc84b9a5d65938fefef2269d9fa9d0f07b64653
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-21 02:43:57 -0400

    Route Handling

--- natours/app.js
+++ natours/app.js
@@ -23,4 +23,13 @@ app.use(express.static(`${__dirname}/public`))
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
+// error handling - for everything - not relatedd to the above routes
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+      status: 'fail',
+      message: `Can't find ${req.originalUrl} on this server!`
+    });
+  });
+
+
 module.exports = app;
