0baae9cd1f4f5c79a7a9172319eecba5291ba393
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-21 03:24:24 -0400

    Using error class

--- natours/app.js
+++ natours/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoute');
 const userRouter = require('./routes/userRoute');
 
@@ -25,16 +26,8 @@ app.use('/api/v1/tours', tourRouter);
 
 // error handling - for everything - not relatedd to the above routes
 app.all('*', (req, res, next) => {
-    // res.status(404).json({
-    //   status: 'fail',
-    //   message: `Can't find ${req.originalUrl} on this server!`
-    // });
-    // using built in function to create an error
-    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-    err.status = 'fail';
-    err.statusCode = 404;
-    // passing error to the next function
-    next(err);
+    
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
   });
 
 // global error handling middleware
