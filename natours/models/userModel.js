fefeab28fc9b22e10af3b47835103d2b8b54e300
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-22 03:21:08 -0400

    Playing with validation

--- natours/models/userModel.js
+++ natours/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 // settimng up the schema
 
@@ -22,14 +23,35 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Please provide a password'],
-        minlength: 8,
+        minlength: 5,
         maxlength: 24
     },
     confirmPassword: {
         type: String,
         required: [true, 'Please confirm your password'],
+        validate: {
+            // this only works on CREATE and SAVE - custom validator - check if the password and confirmPassword are same
+            validator: function(el) {
+                return el === this.password;
+            },
+            message: 'Passwords are not the same'
+        }
     }
 });
+
+// saves the password to db
+userSchema.pre('save', async function(next) {
+    // if the password is not modified then return next
+    if(!this.isModified('password')) return next();
+    // converts the password using the hashing tech
+    this.password = await bcrypt.hashSync(this.password, 12);
+    this.confirmPassword = undefined;
+    next()
+
+
+})
+
+
 
 
 const User = mongoose.model('User', userSchema);
