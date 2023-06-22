b48e45bcb4084075bb983dc10cb9a669826c2abe
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-06-21 23:37:56 -0400

    Improving error handling

--- natours/controller/tourcontroller.js
+++ natours/controller/tourcontroller.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 // Calling it out
 exports.aliasTopTour = (req, res, next) => {
@@ -38,6 +39,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   
     const tour = await Tour.findById(req.params.id);
+    if(!tour){
+      return next(new AppError('No tour found with that ID', 404))
+
+    }
     res.status(200).json({
       status: 'success',
       data: {
@@ -63,6 +68,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
       runValidators: true
     });
 
+    if(!tour){
+      return next(new AppError('No tour found with that ID', 404))
+
+    }
+
     res.status(200).json({
       status: 'success',
       data: {
@@ -73,7 +83,13 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
   
-    await Tour.findByIdAndDelete(req.params.id);
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+
+    if(!tour){
+      return next(new AppError('No tour found with that ID', 404))
+    }
+
+
     res.status(204).json({
       status: 'success',
       data: null,
@@ -117,7 +133,7 @@ exports.getTourStats = catchAsync(async (req, res, next) => {
     });
   });
 
-exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+exports.getMonthlyPlan = catchAsync(async (req, res) => {
 
     const year = req.params.year * 1;
     const plan = await Tour.aggregate([
