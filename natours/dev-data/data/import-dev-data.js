0ac03e9f5f07e8d219e859253de0174c7e155b99
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-07-05 00:43:30 -0400

    Refactoring code

--- natours/dev-data/data/import-dev-data.js
+++ natours/dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const Tour = require('./../../models/tourModel');
+const Review = require('./../../models/reviewModel')
+const User = require('./../../models/userModel')
 
 dotenv.config({ path: './config.env' });
 // SETTING UP DATABASE
@@ -14,12 +16,18 @@ mongoose.connect(DB , {
 }).then(() => console.log('DB connection successful!'));
 
 // read json file 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`,'utf-8')); 
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`,'utf-8')); 
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`,'utf-8')); 
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`,'utf-8')); 
 
 // importing data in db 
 const importData = async () =>{
   try{
     await Tour.create(tours);
+    await User.create(users,{
+      validateBeforeSave : false
+    });
+    await Review.create(reviews);
     console.log('Data loaded properly');
   }catch(err){
     console.log(err);
@@ -29,6 +37,8 @@ const importData = async () =>{
 const deleteData = async() =>{
   try{
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data deleted properly');
   }catch(err){
     console.log(err);
