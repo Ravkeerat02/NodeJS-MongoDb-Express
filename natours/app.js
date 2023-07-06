1f0bd9ac163dcb616c5473ee11ec16197d2e799a
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-07-06 02:15:03 -0400

    Populating template

--- natours/app.js
+++ natours/app.js
@@ -63,9 +63,26 @@ app.use(hpp({
 
 
 // API routes
+// render base tempalte
 app.get('/',(req,res)=>{
-    res.status(200).render('base')
+    res.status(200).render('base',{
+        tour: 'The Forest Hiker',
+        user: 'Jane Doe'
+    })
 })
+
+app.get('/overview',(req,res)=>{
+    res.status(200).render('overview',{
+        title: 'All Tours'
+    })
+})
+
+app.get('/tour',(req,res)=>{
+    res.status(200).render('tour',{
+        title: 'The Forest Hiker Tour'
+    })
+})
+
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
