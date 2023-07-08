2c5ba04ad47392e30e942c11727ed31eafa24d9c
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-07-07 23:47:45 -0400

    still not working

--- natours/public/js/login.js
+++ natours/public/js/login.js
@@ -1,5 +1,6 @@
+
 import axios from 'axios';
-import { showAlert } from './alerts';
+import { showAlert } from './alert';
 
 export const login = async (email, password) => {
   try {
@@ -29,7 +30,7 @@ export const logout = async () => {
       method: 'GET',
       url: 'http://127.0.0.1:3000/api/v1/users/logout'
     });
-    if ((res.data.status = 'success')) location.reload(true);
+    if ((res.data.status === 'success')) location.reload(true);
   } catch (err) {
     console.log(err.response);
     showAlert('error', 'Error logging out! Try again.');
