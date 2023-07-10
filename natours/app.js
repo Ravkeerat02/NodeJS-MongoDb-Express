db5651c070ae3ad37f9c43372de9294e3efb816d
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-07-10 03:12:15 -0400

    MAP still works

--- natours/app.js
+++ natours/app.js
@@ -46,7 +46,7 @@ app.options('*', cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
-app.use(helmet());
+// app.use(helmet());
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
